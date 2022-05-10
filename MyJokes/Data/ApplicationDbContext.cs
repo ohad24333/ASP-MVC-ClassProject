ced40fe9d3bae09aa76f3bfd0d2696cc4229d622
אוהד סעדיה <ohad24333@gmail.com>
2022-05-11 00:53:49 +0300

    Add project files.

---
+++ MyJokes/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MyJokes.Models;
+
+namespace MyJokes.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<MyJokes.Models.Joke> Joke { get; set; }
+    }
+}
---
+++ MyJokes/Models/Joke.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyJokes.Models
+{
+    public class Joke
+    {
+        public int id { get; set; }
+
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+    }
+}
